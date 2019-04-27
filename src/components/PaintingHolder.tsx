0cc56b83db00b26c492e685b7fe38d2dfce73657
Mark Warfield <mark.warfield@dreamm.co.uk>
2019-04-27 20:58:28 +0100

    Add modal image support for images

--- src/components/PaintingHolder.tsx
+++ src/components/PaintingHolder.tsx
@@ -1,6 +1,7 @@
-import React, { SFC } from 'react'
+import React, { SFC, useState } from 'react'
 import { Box } from 'rebass'
 import styled from 'styled-components'
+import Modal from 'react-modal'
 
 const StyledUl = styled('ul')`
   padding: 0;
@@ -13,14 +14,52 @@ const StyledUl = styled('ul')`
   }
 `
 
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)'
+  },
+  overlay: { zIndex: 10, backgroundColor: 'rgba(0, 0, 0, 0.7)' }
+}
+
 const PaintingHolder: SFC<{ img: string; title: string; dims: string }> = ({
   img,
   title,
   dims
 }) => {
+  const [isOpen, setOpen] = useState(false)
   return (
     <Box mb={[5]}>
-      <img src={img} style={{ width: '100%' }} alt={title} />
+      <img
+        src={img}
+        onClick={() => {
+          setOpen(!isOpen)
+        }}
+        alt={title}
+        style={{ width: '100%', cursor: 'pointer' }}
+      />
+      {isOpen && (
+        <Modal
+          isOpen={isOpen}
+          onRequestClose={() => {
+            setOpen(!isOpen)
+          }}
+          style={customStyles}
+        >
+          <img
+            className="image"
+            src={img}
+            onClick={() => {
+              setOpen(!isOpen)
+            }}
+            alt={title}
+          />
+        </Modal>
+      )}
       <StyledUl>
         {title}
         <li>{dims}</li>
